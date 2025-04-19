4aa12f2fa798bc1157812ff9e34cfd8f1147f66c
Snehal <singhsnehal.2k1@gmail.com>
2025-04-19 18:08:01 +0530

    Added docstrings to functions in generate.py

--- generate.py
+++ generate.py
@@ -18,6 +18,17 @@ charset = {
 ####### HELPER FUNCS ########
 
 def randomShuffle(lst: list[str]) -> list[str]:
+
+    """
+    Shuffles given list securely, using secrets module instead of the predictable random module.
+
+    Args:
+        lst: List of characters to be shuffled.
+
+    Returns:
+        Shuffled list.
+    """
+
     lst_len = len(lst)
     range_lst = list(range(lst_len))
     shuffled_lst = []
@@ -29,6 +40,17 @@ def randomShuffle(lst: list[str]) -> list[str]:
 
 
 def allowedSymbols(string: str) -> str:
+
+    """
+    Excludes user provided characters from default symbols.
+
+    Args:
+        string: String of symbols to exclude from default symbols.
+
+    Returns:
+        String of symbols which can be used in the password.
+    """
+
     local_symbols = symbols
     string = str(set(string))
     for char in string:
@@ -39,6 +61,18 @@ def allowedSymbols(string: str) -> str:
 
 
 def generate(include: list[str], exclude: str, length: int):
+
+    """
+    Generates password of given length using characters specified by user.
+
+    Args:
+        include: List of strings containg character sets to include in the password.
+        exclude: String of symbols to exclude from Symbol/Punctuations.
+        length: Length of password to generate.
+
+    Returns:
+        Password as a string.
+    """
 
     if ('punc' in include) and len(exclude):
         charset['punc'] = allowedSymbols(exclude)
@@ -54,7 +88,10 @@ def generate(include: list[str], exclude: str, length: int):
             +
             [secrets.choice(allowed_chars) for _ in range(length - len(include))]
         )
-
     shuffled_lst = randomShuffle(unshuffled_lst)
     password_generated = ''.join(shuffled_lst)
-    print(password_generated)
+
+    return password_generated
+
+#if __name__=="__main__":
+#    password = generate()
