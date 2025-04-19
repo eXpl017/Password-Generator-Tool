0711805b1641e6820bbc065adfaf36e04956e6bd
Snehal <singhsnehal.2k1@gmail.com>
2025-04-19 17:47:49 +0530

    Major changes
    - added custom methods in tool.py to process passed input accordingly
    - added password generation code and custom functions in generate.py
    - added gitignore

--- generate.py
+++ generate.py
@@ -1,13 +1,60 @@
+import secrets
+import string
 
-##################################
-def checkExcluded(string):
+
+############ VARS ###########
+
+symbols = '!@#$%^&~'
+charset = {
+        'lowr': string.ascii_lowercase,
+        'uppr': string.ascii_uppercase,
+        'dig': string.digits,
+        'punc': symbols
+    }
+
+#############################
+
+
+####### HELPER FUNCS ########
+
+def randomShuffle(lst: list[str]) -> list[str]:
+    lst_len = len(lst)
+    range_lst = list(range(lst_len))
+    shuffled_lst = []
+    while len(range_lst):
+        random_pos = secrets.choice(range_lst)
+        shuffled_lst.append(lst[random_pos])
+        range_lst.remove(random_pos)
+    return shuffled_lst
+
+
+def allowedSymbols(string: str) -> str:
     local_symbols = symbols
     string = str(set(string))
-    unavailable = ''
     for char in string:
         if char in local_symbols: local_symbols = local_symbols.replace(char,'')
-        else: unavailable += char
-    if unavailable:
-        print(f'Following characters are not included in the charset and will hence be ignored: {unavailable}')
     return local_symbols
-####################################
+
+#############################
+
+
+def generate(include: list[str], exclude: str, length: int):
+
+    if ('punc' in include) and len(exclude):
+        charset['punc'] = allowedSymbols(exclude)
+
+    allowed_chars = ''
+    for i in include:
+        allowed_chars += charset[i]
+
+    print(allowed_chars)
+
+    unshuffled_lst = (
+            [secrets.choice(charset[x]) for x in include]
+            +
+            [secrets.choice(allowed_chars) for _ in range(length - len(include))]
+        )
+
+    shuffled_lst = randomShuffle(unshuffled_lst)
+    password_generated = ''.join(shuffled_lst)
+    print(password_generated)
--- tool.py
+++ tool.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+from generate import generate
 
 
 ############# VARS #############
@@ -8,6 +9,27 @@ symbols = '!@#$%^&~'
 include_help = 'Charsets to include in the password. "upper", "dig", "punc" refer to Uppercase [A-Z], Digits [0-9] and Symbols, respectively.'
 exclude_help = "Symbols to exclude from generated password. Must be a single string including all symbols to exclude, contained inside 'single quotes' (to be wary of bash special variables and substitution)."
 length_help = "Length of password to generate. Must be between 8 and 20, including both edges."
+
+################################
+
+
+########## CLASS METHOD OVERRIDE #########
+
+class MyAction(argparse.Action):
+
+    def __init__(self, option_strings, dest, **kwargs):
+        if 'default' not in kwargs:
+            kwargs['default'] = ['lowr']
+        super().__init__(option_strings, dest, **kwargs)
+
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        initial_list = ['lowr']
+        if values:
+            final_list = initial_list + [x for x in values]
+        else:
+            final_list = initial_list
+        setattr(namespace, self.dest, final_list)
 
 ################################
 
@@ -51,8 +73,9 @@ parser = argparse.ArgumentParser(prog='pass-gen', description="Tool to generate 
 parser.add_argument(
         '-include',
         nargs='+',
-        default=['uppr'],
+        #default=None,
         choices=['uppr','punc','dig'],
+        action=MyAction,
         help=include_help
     )
 parser.add_argument(
@@ -76,4 +99,6 @@ args = parser.parse_args()
 
 print(args.include)
 print(args.exclude)
-print(args.length)
+print(type(args.length))
+
+generate(args.include, args.exclude, args.length)
